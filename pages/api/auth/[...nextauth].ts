860ccbd46ef11bb8e662e29e6265ea7b6c02f932
Daniel Castro <daniel.castro@digitalhub.man>
2022-06-22 22:11:12 +0100

    Add CredentialsProvider and login (authorize) method
    Video 4

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
 import dbConnect from "../../../lib/dbConnect";
+import User from "../../../model/User";
+import { compare } from "bcrypt";
 
 export default NextAuth({
   providers: [
@@ -10,10 +13,58 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    // Email & Password
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+
+        // Find user with the email
+        const user = await User.findOne({
+          email: credentials?.email,
+        });
+
+        // Email Not found
+        if (!user) {
+          throw new Error("Email is not registered");
+        }
+
+        // Check hased password with DB hashed password
+        const isPasswordCorrect = await compare(
+          credentials!.password,
+          user.hashedPassword
+        );
+
+        // Incorrect password
+        if (!isPasswordCorrect) {
+          throw new Error("Password is incorrect");
+        }
+
+        return user;
+      },
+    }),
   ],
   pages: {
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
   adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
