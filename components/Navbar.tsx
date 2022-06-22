04e720063e3164da8489d433af53e20360214bb5
Daniel Castro <daniel.castro@digitalhub.man>
2022-06-22 21:12:29 +0100

    Style auth page
    Start of Video 4

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,15 @@
 import NextLink from "next/link";
 import { Flex, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
-  return (
+  const { pathname } = useRouter();
+
+  const pagesWithoutNavbar = ["/auth"];
+
+  const renderNavbar = !pagesWithoutNavbar.includes(pathname);
+
+  return renderNavbar ? (
     <Flex justifyContent="space-around" p={20}>
       <Button>
         <NextLink href="/">Home</NextLink>
@@ -14,7 +21,7 @@ const Navbar = () => {
         <NextLink href="/auth">Auth</NextLink>
       </Button>
     </Flex>
-  );
+  ) : null;
 };
 
 export default Navbar;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,46 +1,248 @@
+import { useState } from "react";
 import type { NextPage } from "next";
-import { useSession, signIn, signOut, getProviders } from "next-auth/react";
-import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { useSession, signIn, getProviders } from "next-auth/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+import axios from "axios";
+import Router from "next/router";
+
+const Background = ({ children }: any) => (
+  <Box
+    display="flex"
+    flex="1 1 auto"
+    justifyContent="center"
+    alignItems="center"
+    backgroundImage="url('/blurry-gradient-haikei.svg')" // coming from public folder
+    backgroundSize="cover"
+    backgroundRepeat="no-repeat"
+    backgroundPosition="center"
+    backgroundAttachment="fixed"
+    width="100%"
+    height="100vh"
+    color="white"
+  >
+    {children}
+  </Box>
+);
+
+interface IDivicerProps {
+  word?: string;
+}
+
+const Divider = ({ word }: IDivicerProps) => {
+  return (
+    <>
+      {word ? (
+        <Flex
+          w="100%"
+          alignItems="center"
+          justifyContent="center"
+          gap={2}
+          mb={4}
+        >
+          <Box w="100%" border="solid" borderBottom={2} rounded="full"></Box>
+          <Text>Or</Text>
+          <Box w="100%" border="solid" borderBottom={2} rounded="full"></Box>
+        </Flex>
+      ) : (
+        <Box
+          w="100%"
+          border="solid"
+          borderBottom={2}
+          rounded="full"
+          mb={6}
+        ></Box>
+      )}
+    </>
+  );
+};
 
 const Auth: NextPage = ({ providers }: any) => {
   const { data: session } = useSession();
+  const [authType, setAuthType] = useState("Login");
+  const oppAuthType: { [key: string]: string } = {
+    Login: "Register",
+    Register: "Login",
+  };
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const ProvidersButtons = ({ providers }: any) => (
-    <>
+    <Flex direction="column" w="100%">
       {Object.values(providers).map(
         (provider: any) =>
           provider.name !== "Credentials" && (
             <Button
               key={provider.name}
+              mb={4}
+              bg={"#24292E"}
+              color={"white"}
+              _hover={{ bg: "#24292E90" }}
               type="submit"
               onClick={() => {
                 signIn(provider.id, {
-                  callbackUrl: `http://localhost:3000/`,
+                  callbackUrl: `${process.env.URL_DEV}/`,
                 });
               }}
             >
-              Sign in with {provider.name}
+              <Box>Sign in with {provider.name}</Box>
             </Button>
           )
       )}
-    </>
+    </Flex>
   );
 
+  const redirectToHome = () => {
+    const { pathname } = Router;
+    if (pathname === "/auth") {
+      // TODO: redirect to a success register page
+      Router.push("/");
+    }
+  };
+
+  const registerUser = async () => {
+    const res = await axios
+      .post(
+        "/api/register",
+        { username, email, password },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(async () => {
+        await loginUser();
+        redirectToHome();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(res);
+  };
+
+  const loginUser = async () => {
+    const res: any = await signIn("credentials", {
+      redirect: false,
+      email: email,
+      password: password,
+      callbackUrl: `${window.location.origin}`,
+    });
+
+    res.error ? console.log(res.error) : redirectToHome();
+  };
+
+  const formSubmit = (actions: any) => {
+    actions.setSubmitting(false);
+
+    authType === "Login" ? loginUser() : registerUser();
+  };
+
   return (
-    <Flex direction="column" alignItems="center">
-      <Heading>Auth</Heading>
-      {session ? (
-        <>
-          <Text>Signed in as {session?.user?.email}</Text>
-          <Button onClick={() => signOut()}>Sign out</Button>
-        </>
-      ) : (
-        <>
-          <Text>Not signed in</Text>
+    <Background>
+      <Box
+        w="420px"
+        rounded="md"
+        bgGradient="linear(to-r, #ffffff80, #ffffff20)"
+        p={12}
+      >
+        <Flex direction="column" justifyContent="center" alignItems="center">
+          <Heading size="xl">{authType}</Heading>
+          <Text fontSize="sm" mb={6}>
+            {authType === "Login"
+              ? "Not registered yet? "
+              : "Already have an account? "}
+            <button onClick={() => setAuthType(oppAuthType[authType])}>
+              <Text as="u">{oppAuthType[authType]}</Text>
+            </button>
+          </Text>
+
+          <Divider />
+
           <ProvidersButtons providers={providers} />
-        </>
-      )}
-    </Flex>
+
+          <Divider word="Or" />
+
+          <Formik
+            initialValues={{}} // { email: "", password: "" }
+            validateOnChange={false}
+            validateOnBlur={false}
+            onSubmit={(_, actions) => {
+              formSubmit(actions);
+            }}
+          >
+            {(props) => (
+              <Form style={{ width: "100%" }}>
+                <Box display="flex" flexDirection="column" w="100%" mb={4}>
+                  {authType === "Register" && (
+                    <Field name="username">
+                      {() => (
+                        <FormControl isRequired mb={6}>
+                          <FormLabel htmlFor="username">Username:</FormLabel>
+                          <Input
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Username"
+                            background={"blue.600"}
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                  )}
+                  <Field name="email">
+                    {() => (
+                      <FormControl isRequired mb={6}>
+                        <FormLabel htmlFor="email">Email:</FormLabel>
+                        <Input
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          placeholder="Email Address"
+                          background={"blue.600"}
+                        />
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {() => (
+                      <FormControl isRequired mb={3}>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <Input
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                          type="password"
+                          placeholder="Password"
+                          background={"blue.600"}
+                        />
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Button
+                    mt={6}
+                    bg="blue.400"
+                    _hover={{ bg: "blue.200" }}
+                    isLoading={props.isSubmitting}
+                    type="submit"
+                  >
+                    {authType}
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
+        </Flex>
+      </Box>
+    </Background>
   );
 };
 
